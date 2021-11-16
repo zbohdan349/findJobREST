efa7fbcc8c75129e1481d563fbfa6edd3ad29d70
zbohdan <zbogdan349@gmail.com>
2021-11-16 19:49:50 +0200

    second commit

---
+++ src/main/java/com/findJob/app/controller/StartController.java
@@ -0,0 +1,13 @@
+package com.findJob.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+
+    @GetMapping("/")
+    public  String start(){
+        return "index";
+    }
+}
