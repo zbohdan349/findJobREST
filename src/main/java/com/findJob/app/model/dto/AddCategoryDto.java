85852750bc802c3d0f3b39b2c562ea03afc9bf74
Bobo <zbogdan349@gmail.com>
2022-12-10 17:17:54 +0300

    addCategory

---
+++ src/main/java/com/findJob/app/model/dto/AddCategoryDto.java
@@ -0,0 +1,13 @@
+package com.findJob.app.model.dto;
+
+public class AddCategoryDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
